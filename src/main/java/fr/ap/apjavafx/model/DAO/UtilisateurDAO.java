6d18f3320b6a7a28c991bd777e1fcdc5ca969168
Anthonydt16 <84806723+Anthonydt16@users.noreply.github.com>
2021-11-26 16:44:40 +0100

    statut adherent

--- src/main/java/fr/ap/apjavafx/model/DAO/UtilisateurDAO.java
+++ src/main/java/fr/ap/apjavafx/model/DAO/UtilisateurDAO.java
@@ -1,17 +1,34 @@
 package fr.ap.apjavafx.model.DAO;
 
+import fr.ap.apjavafx.model.DTO.Adherent;
+import fr.ap.apjavafx.model.DTO.Utilisateur;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UtilisateurDAO {
 
 	/**
 	 * Métode permettant de récupérer les informations relatives à un utilisateur
-	 * @param id (id utilidsateur)
-	 * @return ResultSet
-	 */
-//	public static ResultSet unUtilisateur(String id)
-//	{
-//		String requete = "SELECT *  FROM utilisateur where  id = '" + id + "'";
-//		return DBConnex.getRS(requete, DBConnex.connexion());
-//	}
+	 * @param user (utilidsateur)
+     */
+	public static void statut(Utilisateur user) throws SQLException {
+
+		try(PreparedStatement statement = DBConnex.getConnexion().prepareStatement("select I.* from utilisateur as U, inscrit as I where U.login = I.login and U.login = ?")) {
+			statement.setString(1, user.getLOGIN());
+			try(ResultSet result = statement.executeQuery()) {
+				if (result.next()) {
+					if(result != null){
+						// ajout dans l'objet adherent
+					Adherent unAdherent = new Adherent(result.getString(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getString(6), result.getString(7),result.getString(8));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+	}
+	}
 }
