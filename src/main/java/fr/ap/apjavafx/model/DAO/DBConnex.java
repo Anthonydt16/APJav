5e7c2ebd99e4240b76ac5b00165f170282111e6e
Anthonydt16 <84806723+Anthonydt16@users.noreply.github.com>
2021-11-26 15:10:14 +0100

    connexion

--- src/main/java/fr/ap/apjavafx/model/DAO/DBConnex.java
+++ src/main/java/fr/ap/apjavafx/model/DAO/DBConnex.java
@@ -1,62 +1,61 @@
 package fr.ap.apjavafx.model.DAO;
 
 import fr.ap.apjavafx.lib.settings;
+import fr.ap.apjavafx.model.DTO.Utilisateur;
+import jdk.jshell.execution.Util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DBConnex {
 	/**
 	 * M�thode de connexion � la base de donn�es
-	 * @return  Statement
+	 *
+	 * @return connexion
 	 */
-	public static Statement connexion() {
-		
-		Statement statement = null;
-		 try {
-				Connection	connection = DriverManager.getConnection("jdbc:mysql://"+settings.db_host+"/"+settings.db_url+"?user="+ settings.db_user +"&password="+ settings.db_password + "");
-			
-				statement = connection.createStatement();
-				
-				
-				return statement;
-				
-		    } catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();	
-				System.out.println(e.getMessage()); 
-				return statement;
-			}
+	public static Connection getConnexion() throws SQLException {
+
+
+		Connection connexion = DriverManager.getConnection("jdbc:mysql://" + settings.db_host + "/" + settings.db_url + "?user=" + settings.db_user + "&password=" + settings.db_password + "");
+
+		return connexion;
+
+
 	}
-	
+
 	/**
 	 * M�thode d'authentification des l'utilisteur
+	 *
 	 * @param login
 	 * @param mdp
-	 * @param unStatement
-	 * @return ResultSet
 	 */
-	public static ResultSet  authentification(String login , String mdp, Statement unStatement) {
-		
-		ResultSet rs = null ;
-		try {
-			String sql ="";
-			rs = unStatement.executeQuery(sql);
-			if (!rs.next()) {
-				rs =null;
+	public static Utilisateur authentification(String login, String mdp) {
+
+		try(PreparedStatement statement = DBConnex.getConnexion().prepareStatement("SELECT * FROM `utilisateur` where LOGIN = ? and PASSWORD = ?")) {
+			statement.setString(1, login);
+			statement.setString(2, mdp);
+			try(ResultSet result = statement.executeQuery()) {
+
+
+				if (result.next()) {
+					System.out.println("teste pt");
+					Utilisateur unUser = new Utilisateur(result.getString(1), result.getString(2), result.getString(3), result.getString(4), result.getString(5), result.getString(6), result.getString(7));
+					return unUser;
+				}
+
+
 			}
-			
+
 		} catch (SQLException e) {
-				e.printStackTrace();
-				System.out.println(e.getMessage()); 
+			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
-		 return rs;
+		return null;
 	}
-	
-	
+
+
+
+
 	/**
 	 * M�thode permettant l'envoi de requ�tes "select" � la base de donn�es
 	 * @param requete
